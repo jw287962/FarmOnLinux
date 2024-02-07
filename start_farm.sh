e1f882f99a57fe36e4f099a4c9324f854bde10f8
Jason <jasonwong287962@gmail.com>
2024-02-06 23:08:52 -0600

    messages when farm crashes or restarts

--- start_farm.sh
+++ start_farm.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 source functions.sh
+URL=$(get_telegram)
+IP_ADDRESS=$(get_ipaddress)
 
-#FARM=$(grep 'FARMER:' config.yaml | awk -F "'" '{print $2}')
+CHAT_ID=6919531958
+
 FARM=$(get_farm)
 
 LOG_FILE="script.log"
@@ -12,9 +15,9 @@ while true; do
     # Run the script and redirect its output to the log file]
     $FARM > "$LOG_FILE" 2>&1 
 
-
+    curl -X POST "$URL" -d "chat_id=$CHAT_ID" -d "text=RESTARTING CLI" -d "parse_mode=HTML"
     echo "restarting cli....."
-    sleep 1
+    sleep 5
 done
 
 
