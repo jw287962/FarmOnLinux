19d860f11ee68a46b568278780127b7ca036ed46
Jason <jasonwong287962@gmail.com>
2024-02-08 01:14:40 -0600

    minor changes and improvements

--- farm_alerts.sh
+++ farm_alerts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source functions.sh
+
 URL=$(get_telegram)
 IP_ADDRESS=$(get_ipaddress)
 
@@ -8,15 +9,25 @@ CHAT_ID=6919531958
 while true; do
     # LOG TELEGRAM ALERT
     LOG_FILE="script.log"
-    # Get the last log message
-    OUTPUT=$(tail -n 1 "$LOG_FILE")
+
+
+    LIMITLINES=4
+    for i in {1..3}; do
+        OUTPUT=$(tail -n $LIMITLINES "$LOG_FILE")
+        clear
+        echo "Last $LIMITLINES Message:"
+        echo "$OUTPUT"
+        sleep 100
+    done
 
     # Send the last log message via Telegram
-    curl -X POST "$URL" -d "chat_id=$CHAT_ID" -d "text=$OUTPUT" -d "parse_mode=HTML"
-
+    OUTPUT=$(tail -n 1 "$LOG_FILE")
+   
+    curl -X POST "$URL" -d "chat_id=$CHAT_ID" -d "text=RIG 8X $OUTPUT" -d "parse_mode=HTML"
+    
     # Check if the script has exited with a non-zero status
     #
-    # Sleep for 5 minutes
-    sleep 500
+    # Sleep 
+    sleep 10
 done
 
--- gpusmi.sh
+++ gpusmi.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-
+# for NVIDIA GPU mem
 /usr/bin/nvidia-smi -lmc 810;
 
-/usr/bin/nvidia-smi -lgc 1470;
+
+# for 4090
+/usr/bin/nvidia-smi --ID=0 -lgc 2370;
+
+# for 3080s
+/usr/bin/nvidia-smi --ID=1,2,3 -lgc 1470;
+
+
+
+# RUN ALPH
+
+/home/jasonminer/Downloads/rigel-1.14.2-linux/alph.sh
+
