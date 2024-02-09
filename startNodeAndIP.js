36aa126ace2f18a47df64140375754cf1dc0269f
Jason <jasonwong287962@gmail.com>
2024-02-09 11:22:29 -0600

    added loop counter to call startSession for restart TMUX if constant error output

--- startNodeAndIP.js
+++ startNodeAndIP.js
@@ -4,11 +4,22 @@ const config = require('./config')
 const fs = require('fs');
 const { exec } = require('child_process');
 const LOG_FILE = 'node.log'; 
-
+let loopCounter = 0;
 main();
 async function main(){
-        console.log(config.RUNNODE)
+    config.sleep(2000)
+    
     while (config.RUNNODE == 1) {
+        if (loopCounter >=3){
+            exec('/home/jason/Documents/FarmOnLinux/startup/startSession.sh', (error, stdout, stderr) => {
+                if (error) {
+                    console.error('Error executing startSession.sh:', error);
+                    return;
+                }
+                console.log('stdout:', stdout);
+                // console.error('stderr:', stderr);
+            });
+        }
         console.clear();
         await config.sendTelegramMessage(`IP: ${config.getIpAddress()} \n Starting Node`)
         console.log('starting Node');
@@ -16,6 +27,7 @@ async function main(){
         await config.deleteLogFile(LOG_FILE);
         await runNode();
         await config.sleep(10000); // 10 seconds delay
+        loopCounter++;
     }
 
 }
@@ -23,8 +35,9 @@ async function main(){
 
 async function runNode() {
     try {
+        
         const childProcess = exec(config.NODE);
-       
+        
         let errorCount = 0;
         let lastMessageSentTime = Date.now();
 
@@ -33,7 +46,7 @@ async function runNode() {
         childProcess.stdout.on('data', async (data) => {
             fs.appendFileSync(LOG_FILE, data);
             console.log(data);
-            
+                loopcounter = 0;
             if (Date.now() - lastMessageSentTime >= (config.TIMER || 10) * 60 * 1000) {
                 // Send message to Telegram
                 lastMessageSentTime = Date.now();
@@ -53,6 +66,7 @@ async function runNode() {
             errorCount++;
             if (errorCount >=3){
                 childProcess.kill()
+                
                 
             }
         });
