a7aa654fbe28cc88138873a85df90e6a47ec0dc9
Jason <jasonwong287962@gmail.com>
2024-02-10 00:14:33 -0600

    removed minor things that is not needed

--- startNodeAndIP.js
+++ startNodeAndIP.js
@@ -4,22 +4,10 @@ const config = require('./config')
 const fs = require('fs');
 const { exec } = require('child_process');
 const LOG_FILE = 'node.log'; 
-let loopCounter = 0;
 main();
 async function main(){
-    config.sleep(2000)
     
     while (config.RUNNODE == 1) {
-        if (loopCounter >=3){
-            exec('/home/jason/Documents/FarmOnLinux/startup/startSession.sh', (error, stdout, stderr) => {
-                if (error) {
-                    console.error('Error executing startSession.sh:', error);
-                    return;
-                }
-                console.log('stdout:', stdout);
-                // console.error('stderr:', stderr);
-            });
-        }
         console.clear();
         await config.sendTelegramMessage(`IP: ${config.getIpAddress()} \n Starting Node`)
         console.log('starting Node');
@@ -27,7 +15,6 @@ async function main(){
         await config.deleteLogFile(LOG_FILE);
         await runNode();
         await config.sleep(10000); // 10 seconds delay
-        loopCounter++;
     }
 
 }
@@ -38,7 +25,6 @@ async function runNode() {
         
         const childProcess = exec(config.NODE);
         
-        let errorCount = 0;
         let lastMessageSentTime = Date.now();
 
         // config.ensureDirectoryExistence(LOG_FILE)
@@ -46,13 +32,11 @@ async function runNode() {
         childProcess.stdout.on('data', async (data) => {
             fs.appendFileSync(LOG_FILE, data);
             console.log(data);
-                loopcounter = 0;
+
             if (Date.now() - lastMessageSentTime >= (config.TIMER || 10) * 60 * 1000) {
                 // Send message to Telegram
                 lastMessageSentTime = Date.now();
                 await config.sendTelegramMessage(data);
-                
-                // Update the last message sent time
                 
             }
         });
@@ -66,7 +50,6 @@ async function runNode() {
             errorCount++;
             if (errorCount >=3){
                 childProcess.kill()
-                
                 
             }
         });
