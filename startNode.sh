794d108e8542bdb087383b13e54548024414ae71
Jason <jasonwong287962@gmail.com>
2024-02-18 14:43:22 -0600

    startNode directly with basic while loop forever

---
+++ startNode.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/home/jason/.local/bin/subspace-node run --base-path /home/jason/.local/share/SubspaceNode --chain gemini-3h --farmer --name 'Jason' --rpc-methods unsafe --rpc-cors all --rpc-listen-on 0.0.0.0:9945 --prometheus-listen-on 0.0.0.0:1111
