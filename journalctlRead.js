7ef6c56d373aa6cfa2005e10cba2eab745bca909
Jason <jasonwong287962@gmail.com>
2024-03-18 23:55:23 -0500

    logs systemD to telegram & log file

--- journalctlRead.js
+++ journalctlRead.js
@@ -1,5 +1,5 @@
 const config = require('./config');
-// const fs = require('fs');
+const fs = require('fs');
 const { exec } = require('child_process');
 
 const LOG_FILE = 'farmers.log';
@@ -12,10 +12,17 @@ async function main() {
     console.log('STARTING FARM');
     await config.sendTelegramMessage('STARTING FARM');
 
-    // config.ensureDirectoryExistence(LOG_FILE);
-    // await config.deleteLogFile(LOG_FILE);
+    config.ensureDirectoryExistence(LOG_FILE);
+    await config.deleteLogFile(LOG_FILE);
         
     await runFarmer();
+    fs.rename(LOG_FILE, farmers.backup.log, (err) => {
+        if (err) {
+          console.error('Error renaming file:', err);
+        } else {
+          console.log('File renamed successfully.');
+        }
+      });
 }
 
 async function runFarmer() {
@@ -24,7 +31,7 @@ async function runFarmer() {
         let lastMessageSentTime = Date.now();
 
         childProcess.stdout.on('data', async (data) => {
-            // fs.appendFileSync(LOG_FILE, data);
+             fs.appendFileSync(LOG_FILE, data);
             console.log(data);
 
             if (Date.now() - lastMessageSentTime >= config.TIMER * 60 * 1000) {
@@ -34,8 +41,8 @@ async function runFarmer() {
         });
 
         childProcess.stderr.on('data', async (data) => {
-            // fs.appendFileSync(LOG_FILE, data);
-            // console.log(data);
+            fs.appendFileSync(LOG_FILE, data);
+             console.log(data);
             await config.sendTelegramMessage(`ERROR: ${data}`);
             childProcess.kill();
         });
