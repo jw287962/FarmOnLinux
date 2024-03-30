7ffa0ac97e94b69b2d44e59ec7ee377accbe311f
Jason <jasonwong287962@gmail.com>
2024-03-30 11:18:43 -0500

    just rename for now

--- cleanup_logs.sh
+++ cleanup_logs.sh
@@ -3,12 +3,10 @@
 log_file="/home/jason/Documents/FarmOnLinux/$(date +%F)node.log"
 
 # Check if the file exists
-if [ ! -f "$log_file" ]; then
-    touch "$log_file"
-    echo "Log file created: $log_file"
-else
-    echo "Log file already exists: $log_file"
-fi
+
+mv /home/jason/Documents/FarmOnLinux/node.log "$log_file"
+echo "Logs moved to: $log_file"
+
 
 # Remove node.log files older than 3 days
 old_log="/home/jason/Documents/FarmOnLinux/$(date -d "3 days ago" +%F)node.log"
