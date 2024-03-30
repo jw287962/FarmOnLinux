55171b78fe1f4c044bbbd123d38f60e33f15e3a4
Jason <jasonwong287962@gmail.com>
2024-03-30 11:01:42 -0500

    minor fix:

--- cleanup_logs.sh
+++ cleanup_logs.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-# Move node.log to a dated file
-mv /home/jason/Documents/FarmOnLinux/node.log /home/jason/Documents/FarmOnLinux/"$(date +%F)"node.log
+log_file="/home/jason/Documents/FarmOnLinux/$(date +%F)node.log"
+
+# Check if the file exists
+if [ ! -f "$log_file" ]; then
+    touch "$log_file"
+    echo "Log file created: $log_file"
+else
+    echo "Log file already exists: $log_file"
+fi
 
 # Remove node.log files older than 3 days
-rm /home/jason/Documents/FarmOnLinux/"$(date -d "3 days ago" +%F)"node.log
+old_log="/home/jason/Documents/FarmOnLinux/$(date -d "3 days ago" +%F)node.log"
+if [ -f "$old_log" ]; then
+    rm "$old_log"
+    echo "Old log file removed: $old_log"
+fi
 
